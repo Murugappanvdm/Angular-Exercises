13b5cf4d9e229ee3c18ee35152ac85033b78f561
Murugappan Krishnamurthy <mkrishnamur6@dedalus.eu>
2023-06-01 12:21:21 +0530

    Added Exercises

--- api-based-website/script.js
+++ api-based-website/script.js
@@ -1,11 +1,18 @@
-async function quoteRead()
-{
+async function quoteRead() {
     let response = await fetch("https://api.quotable.io/random")
     let data = await response.text()
-    console.log(JSON.parse(data))
+    //console.log(JSON.parse(data))
+    quote = JSON.parse(data)
+    author = quote.author
+    content = quote.content
+    let code = ""
+    code += "<p>" + content + "</p>"
+    code += "<b><i> - " + author + "</i></b>"
+    document.getElementById("quotes-for-the-day").innerHTML = code
 }
-
 quoteRead()
+setInterval(
+    quoteRead, 3000)
 /*response=fetch("https://api.quotable.io/random")
 //return of fetch is promise
 //unless you specify method name in fetch function, it
