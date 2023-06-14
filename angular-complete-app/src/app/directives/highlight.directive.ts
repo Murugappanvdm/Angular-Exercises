b118b7bc7753d72eac712ea694a9ee91dc4c9db1
Murugappan Krishnamurthy <mkrishnamur6@dedalus.eu>
2023-06-14 12:55:54 +0530

    modified for directives

--- angular-complete-app/src/app/directives/highlight.directive.ts
+++ angular-complete-app/src/app/directives/highlight.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener,Input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[highlight]'
 })
 export class HighlightDirective {
+@Input() highlight:string=""
+  //elementRef would get the target elements referenc
+  // <h1 [highlight]="data"> </h1> , in this case elementref would represent h1
+  constructor(private el:ElementRef) { }
 
-  constructor() { }
+  @HostListener("mouseover")
+  doSomething():void {
+    console.log(this.highlight, "working")
+      let element =   this.el.nativeElement
+      element = element.children[1]
+      let regex=/(good|excellent|great|super|extraordinary|satisfied)/
+      if(regex.test(this.highlight.toLowerCase())) {
+        console.log("got inside")
+          element.style.color ="darkred"
+          element.style.fontSize="26px"
+      }
+  }
 
+  @HostListener("mouseleave")
+  turnOriginal():void {
+    let element = this.el.nativeElement.children[1]
+    element.style.color ="rgb(54, 54, 54)"
+    element.style.fontSize="16px"
+  }
+ 
 }
