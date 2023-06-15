50a717565810cf96d54caf23548cd186458307f8
Murugappan Krishnamurthy <mkrishnamur6@dedalus.eu>
2023-06-15 10:57:04 +0530

    Added filter option with event

---
+++ angular-complete-app/src/app/pipes/reviewfilter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { review } from '../models/models';
+
+@Pipe({
+  name: 'reviewfilter'
+})
+export class ReviewfilterPipe implements PipeTransform {
+
+  transform(reviews: review[], projecttype:string): review[] {
+    if(projecttype=="All")
+    return reviews
+    else
+    return reviews.filter(x=>x.type == projecttype)
+  }
+
+}
