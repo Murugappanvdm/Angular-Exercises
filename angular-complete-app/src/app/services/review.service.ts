f43b88ee3add2177a1234341f50d6f7e6a15ab33
Murugappan Krishnamurthy <mkrishnamur6@dedalus.eu>
2023-06-15 13:14:16 +0530

    Added support for template form to capture review

--- angular-complete-app/src/app/services/review.service.ts
+++ angular-complete-app/src/app/services/review.service.ts
@@ -1,16 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs'
+import { review } from '../models/models';
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
-
-  constructor(private http:HttpClient )
-  {
+  httpOptions: any
+  constructor(private http: HttpClient) {
 
   }
-  getReviews():Observable<any> {
-       return this.http.get("http://localhost:4500/feedbacks")
-   }
+  getReviews(): Observable<any> {
+    return this.http.get("http://localhost:4500/feedbacks")
+  }
+  postReview(obj: review): Observable<any> {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post("http://localhost:4500/feedbacks", obj, this.httpOptions)
+  }
 }
