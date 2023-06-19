d903bafd50b4e93fb3b37a47cd17ba5c76bdd76a
Murugappan Krishnamurthy <mkrishnamur6@dedalus.eu>
2023-06-19 17:17:43 +0530

    added code to save form data

--- angular-complete-app/src/app/services/home.service.ts
+++ angular-complete-app/src/app/services/home.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,19 +6,29 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class HomeService {
+  httpOptions: any
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
-  getServices():Observable<any>{
-     return this.http.get("http://localhost:4500/services")   
+  getServices(): Observable<any> {
+    return this.http.get("http://localhost:4500/services")
   }
 
-  getDetails(id:number):Observable<any>
-  {
-    return this.http.get("http://localhost:4500/details/"+id) 
+  getDetails(id: number): Observable<any> {
+    return this.http.get("http://localhost:4500/details/" + id)
   }
 
-  getService(id:number):Observable<any> {
-    return this.http.get("http://localhost:4500/services/"+id) 
+  getService(id: number): Observable<any> {
+    return this.http.get("http://localhost:4500/services/" + id)
+  }
+
+  postEnquiry(obj:any,fflag:boolean):Observable<any> {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    }
+    if(!fflag)
+      return this.http.post("http://localhost:5600/api/enquiries",obj,this.httpOptions)
+    return this.http.post("http://localhost:5600/api/enquiries",obj)
   }
 }
